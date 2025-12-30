1480ca7dd23f881239f27c2cd21eacde5da3f6f6
z3trium <lolchen1510@gmail.com>
2025-12-30 16:37:40 +0100

    First commit of mylibc created by myself

---
+++ lIbC_bYmYs3lF/mylibc.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include "mylibc.h"
+
+
+int main(){
+	char* source = "235";
+	printf("Démarrage du programme...\n");
+	printf("Son type est de %zu octets, et le résultat est %s\n", sizeof(source), source);
+	int result = atointeger(source);
+	printf("Son type est de %zu octets, et le résultat est %d\n", sizeof(result), result);
+	printf("Fin du programme...\n");
+	return 0;
+}
+
+// strlength fonctionnel
+
+int strlength(char* s){
+	int length = 0;
+
+	for (int i = 0; *(s+i) != '\0'; i++){
+		length++;	
+	}
+
+	return length;
+}
+
+// stringcopy fonctionnel
+
+char* stringcopy(char* s1, char* s2){
+	s2 = s1;
+	return s2;
+}
+
+// strcmp fonctionnel (peut-être pas très optimisé)
+
+int stringcmp(char* s1, char* s2){
+	int result = 1;
+	if (strlength(s1) != strlength(s2)){
+		return result;
+	}
+	else if (strlength(s1) == 0 && strlength(s2) == 0){
+		return -1;
+	}
+	for (int i = 0; *(s1+i) != '\0'; i++){
+		if (*(s1+i) == *(s2+i)){
+			result = 0;
+			continue;
+		}
+		result = 1;
+		break;
+	}
+	return result;
+}
+
+//strchr fonctionnel
+
+int stringchr(char* s, char c){
+	for (int i = 0; *(s+i)!='\0'; i++){
+		if (*(s+i) == c){
+			return i+1;
+		}
+	}
+	return -1;
+}
+
+void* memory_copy(void* destination, void* source, int num_bytes){
+	char *p_destination = destination;
+	char *p_source = source;
+	for (int i = 0; i<num_bytes; i++){
+		*(p_destination+i) = *(p_source+i);
+	}
+	return destination;
+}
+
+// atointeger fonctionnel
+
+int atointeger(char* str){
+	int result = 0;
+	for (int i = 0; *(str+i) != '\0'; i++){
+		int digit = *(str+i) - '0';
+		result = result * 10 + digit;
+	}
+	return result;
+}
---
+++ lIbC_bYmYs3lF/mylibc.h
@@ -0,0 +1,12 @@
+#ifndef MYLIBC_H
+#define MYLIBC_H
+
+int strlength(char* s);
+char* stringcopy(char* s1, char* s2);
+int stringcmp(char* s1, char* s2);
+
+int stringchr(char* s, char c);
+void* memory_copy(void* destination, void* source, int num_bytes);
+int atointeger(char* str);
+
+#endif
